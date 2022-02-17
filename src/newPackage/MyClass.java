79164fdc40424b0d06aa84859c9c1144ba09fb71
wjbad14 <wessel.badenhorst@archsoftware.co.za>
2022-02-17 18:52:32 +0200

    Second commit

--- src/newPackage/MyClass.java
+++ src/newPackage/MyClass.java
@@ -1,10 +1,20 @@
 package newPackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//Declaration and instantiation of objects and variables
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "https://demo.guru99.com/test/newtours/";
+		
+		//Launch chrome and direct to base URL
+		driver.get(baseUrl);
 	}
 
 }
